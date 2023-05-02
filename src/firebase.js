094650982d4c38660e8c94c67adc044503eab9e9
Shekhar Sunuwar <129270764+sunuwarshekhar@users.noreply.github.com>
2023-05-02 22:08:06 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7G-QPvVsmm5R0ovmttLwjvf7UnSbgF8s",
-  authDomain: "netflix-d0c13.firebaseapp.com",
-  projectId: "netflix-d0c13",
-  storageBucket: "netflix-d0c13.appspot.com",
-  messagingSenderId: "204016335267",
-  appId: "1:204016335267:web:8387e5569e7c4bb67cc216",
-  measurementId: "G-H7BM4TD935"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 
   //...
 };
